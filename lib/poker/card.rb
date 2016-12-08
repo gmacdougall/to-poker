a99294ec4d9072c220ce25b54f8d364447828679
Allison C. Reilly <acreilly3@gmail.com>
2016-12-08 11:00:53 -0500

    Added constants SUITS and RANKS with specs

--- lib/poker/card.rb
+++ lib/poker/card.rb
@@ -2,6 +2,9 @@ module Poker
   class Card
     attr_reader :rank, :suit
 
+    RANKS = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+    SUITS = ['Hearts', 'Spades', 'Diamonds', 'Clubs']
+
     def initialize(rank, suit)
       @rank = rank
       @suit = suit
--- spec/poker/card_spec.rb
+++ spec/poker/card_spec.rb
@@ -13,4 +13,24 @@ RSpec.describe Poker::Card, type: :model do
 
     expect(card.rank).to eq('Queen')
   end
+
+  describe 'SUITS' do
+    it 'should include Hearts, Diamonds, Clubs, and Spades' do
+      suits = ["Hearts", "Diamonds", "Clubs", "Spades"]
+
+      expect(Poker::Card::SUITS).to include(*suits)
+    end
+  end
+
+  describe 'RANKS' do
+    it 'should include numbers 2 through 10' do
+      expect(Poker::Card::RANKS).to include(*(2..10).to_a.map(&:to_s))
+    end
+
+    it 'should include J, Q, K, A' do
+      face_cards = ['J', 'Q', 'K', 'A']
+
+      expect(Poker::Card::RANKS).to include(*face_cards)
+    end
+  end
 end
