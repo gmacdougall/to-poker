2410bc17242fefa8958cbd1ec9e726459f9ed1b8
Allison C. Reilly <acreilly3@gmail.com>
2016-12-09 09:26:44 -0500

    Move frozen_string_literal to top line

--- lib/poker.rb
+++ lib/poker.rb
@@ -1,9 +1,9 @@
+# frozen_string_literal: true
 require 'pry'
 require 'poker/card'
 require 'poker/deck'
 require 'poker/hand'
 require 'poker/dealer'
-# frozen_string_literal: true
 # Poker Top Level Library
 module Poker
 end
